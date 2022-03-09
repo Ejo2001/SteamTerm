587f37d1276b4e185dc9c0a9c3bdcd4bf9005a97
Ejo2001 <Erikjo2001@gmail.com>
2022-03-09 20:12:13 +0100

    Added the foundation for the python client code

---
+++ main.py
@@ -0,0 +1,72 @@
+import sqlite3
+import requests
+import os
+
+#stream = os.popen(Command)
+#output = stream.read()
+#print(output)
+
+Conn = sqlite3.connect('GameDatabase.db')
+
+Cursor = Conn.cursor()
+
+try:
+    Cursor.execute('''CREATE TABLE Games
+                                      (appid, name, publisher, launchcmd, filename)''')
+except:
+    print("Database already exist")
+
+
+
+
+class game():
+    def __init__(self):
+        self.filename = "NULL"
+        self.appid = "NULL"
+        self.name = "NULL"
+        
+def gamelibrary():
+    stream = os.popen("ls /etc/SteamGames/steamapps")
+    output = stream.read()
+    if output == "":
+        print("No games found")
+    else:
+        print(output)
+    input("Press enter to continue...")
+
+def launch():
+    print("Launch")
+    input("Press enter to continue...")
+
+def install():
+    print("Install")
+    input("Press enter to continue...")
+
+def gamedb():
+    print("gamedb")
+    input("Press enter to continue...")
+
+def updategamedb():
+    print("Updating game database")
+
+
+
+while True:
+    stream = os.popen("clear")
+    output = stream.read()
+    print(output)
+    print("1. List installed games\n2. View game database\n3. Install game\n4. Launch game\n5. Quit")
+    answer = input("Please enter a number: ")
+    try:
+        if int(answer) == 1:
+            gamelibrary()
+        elif int(answer) == 2:
+            gamedb()
+        elif int(answer) == 3:
+            install()
+        elif int(answer) == 4:
+            launch()
+        elif int(answer) == 5:
+            exit()
+    except:
+        print("Please enter a correct input")
--- setup.sh
+++ setup.sh
@@ -3,3 +3,8 @@ add-apt-repository multiverse
 dpkg --add-architecture i386
 apt update
 apt install python3.9 python3-pip steamcmd -y
+pip install psycopg2-binary
+pip install requests
+pip install sqlite3
+pip install os
+steamcmd +force_install_dir /etc/SteamGames +quit
